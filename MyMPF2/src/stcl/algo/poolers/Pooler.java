6179e6b69518750c0e03546b905de14ac13faeba
Simon <Simon@tcpost.dk>
2014-09-20 18:28:36 +0300

    Created new classes. No work done yet

---
+++ MyMPF2/src/stcl/algo/poolers/Pooler.java
@@ -0,0 +1,9 @@
+package stcl.algo.poolers;
+
+public abstract class Pooler {
+
+	public Pooler() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ MyMPF2/src/stcl/algo/poolers/TemporalPooler.java
@@ -0,0 +1,13 @@
+package stcl.algo.poolers;
+
+import java.util.Random;
+
+public class TemporalPooler extends SpatialPooler {
+
+	public TemporalPooler(Random rand, int maxIterations, int inputLength,
+			int mapSize) {
+		super(rand, maxIterations, inputLength, mapSize);
+		// TODO Auto-generated constructor stub
+	}
+
+}

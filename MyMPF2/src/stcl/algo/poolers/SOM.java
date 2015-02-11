740ba0cb31fc2bd817c6600fb372c6088ff586ae
Simon <Simon@tcpost.dk>
2015-02-11 14:55:40 +0200

    Add temporal stability experiment only with RSOM

--- MyMPF2/src/stcl/algo/poolers/SOM.java
+++ MyMPF2/src/stcl/algo/poolers/SOM.java
@@ -7,10 +7,11 @@ import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 import dk.stcl.core.som.SOM_SemiOnline;
 import dk.stcl.core.som.SOM_Simple;
 
-public class SOM extends SOM_SemiOnline {
+public class SOM extends SOM_Simple {
 
 	public SOM(int mapSize, int inputLength, Random rand, int maxIterations,
 			double learningRate, double activationCodingFactor) {
+		
 		super(mapSize, inputLength, rand, maxIterations, learningRate,
 				activationCodingFactor);
 		// TODO Auto-generated constructor stub

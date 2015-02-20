da7de2b6cdb5b455dded8e27ba63aed1b27600f4
Simon <Simon@tcpost.dk>
2015-02-20 14:17:36 +0200

    Add class to label rsoms

---
+++ MyMPF2/src/stcl/fun/temporalRecognition/RSomLabeler.java
@@ -0,0 +1,112 @@
+package stcl.fun.temporalRecognition;
+
+import org.ejml.simple.SimpleMatrix;
+
+import stcl.algo.poolers.SpatialPooler;
+import stcl.algo.poolers.TemporalPooler;
+import dk.stcl.core.basic.containers.SomNode;
+import dk.stcl.core.rsom.IRSOM;
+import dk.stcl.core.som.ISOM;
+
+/**
+ * This class is used for labeling the nodes of a SOM
+ * @author Simon
+ *
+ */
+public class RSomLabeler {
+	
+	/**
+	 * 
+	 * @param rsom
+	 * @param sequences
+	 * @param sequenceLabels
+	 */
+	public void labelSingleRSOM (IRSOM  rsom, SimpleMatrix[][] sequences, int[] sequenceLabels){
+		assert sequences.length == sequenceLabels.length : "The number of labels does not equal the number of sequences!";
+		
+		for (int sequenceID = 0; sequenceID < sequences.length; sequenceID++){
+			rsom.flush();
+			SimpleMatrix[] sequence = sequences[sequenceID];
+						
+			for (SimpleMatrix m : sequence){
+				rsom.step(m);
+			}
+			
+			SomNode bmu = rsom.getBMU();
+			bmu.setLabel(sequenceLabels[sequenceID]);
+		}	
+	}
+	
+	public void labelSOMRSOMPair(ISOM som, IRSOM rsom, SimpleMatrix[][] sequences, int[] sequenceLabels){
+		assert sequences.length == sequenceLabels.length : "The number of labels does not equal the number of sequences!";
+		
+		for (int sequenceID = 0; sequenceID < sequences.length; sequenceID++){
+			rsom.flush();
+			SimpleMatrix[] sequence = sequences[sequenceID];
+						
+			for (SimpleMatrix m : sequence){
+	    		//Spatial classification
+	    		som.step(m);
+	    		SimpleMatrix spatialFFOutputMatrix = som.computeActivationMatrix();
+	    		
+	    		spatialFFOutputMatrix = orthogonalize(spatialFFOutputMatrix);
+	    		
+	    		//Transform spatial output matrix to vector
+	    		SimpleMatrix temporalFFInputVector = new SimpleMatrix(spatialFFOutputMatrix);
+	    		temporalFFInputVector.reshape(1, spatialFFOutputMatrix.getMatrix().data.length);
+	    		
+	    		//Temporal classification
+	    		rsom.step(temporalFFInputVector);
+
+			}
+			
+			SomNode bmu = rsom.getBMU();
+			bmu.setLabel(sequenceLabels[sequenceID]);
+		}	
+
+	}
+	
+	public void labelRSOMInTemporalPooler(SpatialPooler spatialPooler, TemporalPooler temporalPooler, SimpleMatrix[][] sequences, int[] sequenceLabels){
+		assert sequences.length == sequenceLabels.length : "The number of labels does not equal the number of sequences!";
+		
+		for (int sequenceID = 0; sequenceID < sequences.length; sequenceID++){
+			temporalPooler.flushTemporalMemory();
+			SimpleMatrix[] sequence = sequences[sequenceID];
+						
+			for (SimpleMatrix m : sequence){
+	    		//Spatial classification
+	    		SimpleMatrix spatialFFOutputMatrix = spatialPooler.feedForward(m);
+	    		
+	    		//Transform spatial output matrix to vector
+	    		SimpleMatrix temporalFFInputVector = new SimpleMatrix(spatialFFOutputMatrix);
+	    		temporalFFInputVector.reshape(1, spatialFFOutputMatrix.getMatrix().data.length);
+	    		
+	    		//Temporal classification
+	    		temporalPooler.feedForward(temporalFFInputVector);
+
+			}
+			
+			SomNode bmu = temporalPooler.getRSOM().getBMU();
+			bmu.setLabel(sequenceLabels[sequenceID]);
+		}	
+
+	}
+	
+	private SimpleMatrix orthogonalize(SimpleMatrix m) {
+		double max = Double.NEGATIVE_INFINITY;
+		int maxID = -1;
+		for (int i = 0; i < m.getNumElements(); i++){
+			double value = m.get(i);
+			if (value > max){
+				max = value;
+				maxID = i;
+			}
+		}
+		
+		SimpleMatrix ortho = new SimpleMatrix(m.numRows(), m.numCols());
+		ortho.set(maxID, 1);
+		return ortho;
+	}
+
+
+}

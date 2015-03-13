b3adabae1fabd5dda3e89c3c0d559c48ba241d5d
Simon <Simon@tcpost.dk>
2015-03-13 15:47:18 +0200

    Problems with prediction / recognition

--- MyMPF2/src/stcl/fun/rps/RockPaperScissors_WithBrainSimplified.java
+++ MyMPF2/src/stcl/fun/rps/RockPaperScissors_WithBrainSimplified.java
@@ -10,7 +10,7 @@ import stcl.algo.brain.NU;
 import stcl.algo.brain.NeoCorticalUnit;
 import stcl.algo.poolers.RSOM;
 
-public class RockPaperScissors_WithBrain_Simplified {
+public class RockPaperScissors_WithBrainSimplified {
 
 	private Random rand = new Random(1234);
 	private Brain brain;
@@ -22,12 +22,12 @@ public class RockPaperScissors_WithBrain_Simplified {
 	private final int ITERATIONS = 1000;
 	
 	public static void main(String[] args) {
-		RockPaperScissors_WithBrain_Simplified runner = new RockPaperScissors_WithBrain_Simplified();
+		RockPaperScissors_WithBrainSimplified runner = new RockPaperScissors_WithBrainSimplified();
 		runner.run();
 
 	}
 	
-	public RockPaperScissors_WithBrain_Simplified() {
+	public RockPaperScissors_WithBrainSimplified() {
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -39,9 +39,9 @@ public class RockPaperScissors_WithBrain_Simplified {
 	private void setup(int maxIterations){
 		createInputs();
 		createRewardMatrix();
-		int ffInputLength = 2; //Label and action
-		int spatialMapSize = 5;
-		int temporalMapSize = 5;
+		int ffInputLength = rock.numCols() * rock.numRows() * 2; //Multiplying by two to make room for both input and action
+		int spatialMapSize = 3;
+		int temporalMapSize = 2;
 		int markovOrder = 2;
 		brain = new Brain(2, rand, ffInputLength, spatialMapSize, temporalMapSize, markovOrder);
 		
@@ -51,56 +51,83 @@ public class RockPaperScissors_WithBrain_Simplified {
 		int curInput = 0;
 		double externalReward = 0;
 		
-		double[][] tmp = {{0}};
+		double[][] tmp = {{0,0,0}};
+		SimpleMatrix actionNow = new SimpleMatrix(tmp); //m(t)
 		SimpleMatrix actionNext = new SimpleMatrix(tmp); //m(t+1)
+		SimpleMatrix actionAfterNext = new SimpleMatrix(tmp); //m(t+2)
 		int actionIDNow = 0;
 		int actionIDNext = 0;
 		int actionIDAfterNext = 0;
-		
-		SimpleMatrix input = new SimpleMatrix(tmp);
+
+		SimpleMatrix prediction = new SimpleMatrix(tmp);
 		int predictedLabel = 0;
 		
 		for (int i = 0; i < maxIterations; i++){
 			//Update action chain
-			actionIDNow = actionIDNext;
-			actionIDNext = actionIDAfterNext;
-			actionIDAfterNext = -1; //Just to keep track			
+			actionNow = actionNext;
+			actionNext = actionAfterNext;
+			actionAfterNext = null;
 			
-			//Get input
+			//Get input			
+			SimpleMatrix input = new SimpleMatrix(sequence[curInput]);
 			int inputLabel = labelSequence[curInput];
-			input.set(inputLabel);
-
-			//int labelError = predictedLabel == inputLabel ? 0 : 1;		
 			
-			externalReward = reward(inputLabel, actionIDNow);
-			System.out.println(inputLabel + " " + predictedLabel);
+			//Calculate prediction error
+			SimpleMatrix diff = input.minus(prediction);
+			double predictionError = diff.normF();
 			
-			//System.out.println(i + " " + inputLabel + " " + actionIDNow);
-			//System.out.println("Iteration " + i + " Reward: " + externalReward);
-			//System.out.println("Iteration " + i + " labelError: " + labelError);
+			//Calculate reward
 			
-			//System.out.println(i + " " + externalReward);
-
-			//Combine with action vector
-			actionNext.set(actionIDNext);
+			
+			//Combine input with the action that will be performed at time t+1
 			SimpleMatrix combinedInput = input.combine(0, input.END, actionNext);
 			
-			SimpleMatrix fbOutput = brain.step(combinedInput, externalReward);
+			//Give combined input to brain and collect output
+			SimpleMatrix output = brain.step(combinedInput, externalReward);
 			
-			//Collect action that will be done at timestep t + 2	
+			//Extract prediction
+			prediction = output.extractMatrix(0, 1, 0, 3);
 			
-			actionIDAfterNext = Math.round(Math.round(fbOutput.get(fbOutput.getNumElements() - 1)));
+			//Extract action to perform at t+2
+			actionAfterNext = output.extractMatrix(0, 1, 3, output.END);
 			
-			//Collect prediction and label of input that we expect to see at timestep t + 1
-			predictedLabel = Math.round(Math.round(fbOutput.get(fbOutput.getNumElements() - 2)));			
+			//Set max value of action to 1. The rest to zero
+			int max = maxID(actionAfterNext);
+			if (max != -1){
+				actionAfterNext.set(0);
+				actionAfterNext.set(max, 1);
+			}
+			
+			//Print info
+			System.out.println(i + " Error: " + predictionError);
 			
 			curInput++;
 			if (curInput >= sequence.length){
 				curInput = 0;
 			}
+			
 		}
 		
-		//printInformation();
+		printInformation();
+	}
+	
+	private int maxID(SimpleMatrix m){
+		//Transform bias matrix into vector
+				double[] vector = m.getMatrix().data;
+				
+				//Go through bias vector until value is >= random number
+				double max = Double.NEGATIVE_INFINITY;
+				int maxID = -1;
+				int id = 0;
+				
+				for (double d : vector){
+					if (d > max){
+						max = d;
+						maxID = id;
+					}
+					id++;
+				}
+				return maxID;
 	}
 	
 	private void printInformation(){
@@ -144,31 +171,19 @@ public class RockPaperScissors_WithBrain_Simplified {
 
 	private void createInputs(){
 		double[][] rockData = {
-				{0,0,0,0,0},
-				{0,1,1,1,0},
-				{0,1,1,1,0},
-				{0,1,1,1,0},
-				{0,0,0,0,0}
+				{1,0,0}
 		};
 		
 		rock = new SimpleMatrix(rockData);
 		
 		double[][] paperData = {
-				{1,1,1,1,1},
-				{1,1,1,1,1},
-				{1,1,1,1,1},
-				{1,1,1,1,1},
-				{1,1,1,1,1}
+				{0,1,0}
 		};
 		
 		paper = new SimpleMatrix(paperData);
 		
 		double[][] scissorsData = {
-				{0,0,0,1,0},
-				{1,0,1,0,0},
-				{0,1,0,0,0},
-				{1,0,1,0,0},
-				{0,0,0,1,0}
+				{0,0,1}
 		};
 		
 		scissors = new SimpleMatrix(scissorsData);

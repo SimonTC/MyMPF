816394033ed573835925e6997540468330211bc8
Simon <Simon@tcpost.dk>
2015-03-13 15:14:40 +0200

    Bitstrings can now be returned as double arrays

--- MyMPF2/src/stcl/algo/util/DataConverter.java
+++ MyMPF2/src/stcl/algo/util/DataConverter.java
@@ -8,13 +8,22 @@ public class DataConverter {
 	 * @param length length of bit string. 0's will be added in front of the bit string if it is not long enough
 	 * @return
 	 */
-	public static String intToBitString(int i, int length){
-		String s = Integer.toBinaryString(i);
+	public static String intToBitString(int integer, int length){
+		String s = Integer.toBinaryString(integer);
 		while (s.length() < length){
 			s = "0" + s;
 		}
 		
 		return s;
+	}
+	
+	public static double[] intToBitStringDouble(int integer, int length){
+		String bitString = intToBitString(integer, length);
+		double[] arr = new double[length];
+		for (int i = 0; i < bitString.length(); i++){
+			arr[i] = Double.parseDouble(bitString.substring(i, i+1));
+		}
+		return arr;
 	}
 	
 	/**

da93bdd3355d93a6b69ba4d31160da1d70106a29
Simon <Simon@tcpost.dk>
2015-03-24 14:14:19 +0200

    Add possibility of exploration to Decider

--- MyMPF2/src/stcl/algo/predictors/Decider.java
+++ MyMPF2/src/stcl/algo/predictors/Decider.java
@@ -15,10 +15,11 @@ public class Decider extends Predictor_VOMM {
 	private SimpleMatrix correlationMatrix;
 	private RewardFunction rewardFunction;
 	private double externalReward;
+	private Random rand;
 
 	public Decider(int markovOrder, double learningRate, Random rand, double biasInfluence, double maxReward, double alpha, int inputMatrixSize) {
 		super(markovOrder, learningRate, rand);
-		
+		this.rand = rand;
 		this.bias = new BiasUnit(inputMatrixSize, biasInfluence, rand);
 		this.correlator = new RewardCorrelator(inputMatrixSize);
 		this.rewardFunction = new RewardFunction(maxReward, alpha);
@@ -31,6 +32,10 @@ public class Decider extends Predictor_VOMM {
 	 * Biases the prediction towards a state that leads to a higher reward (exploitation) or a random state (exploration)
 	 */
 	public SimpleMatrix predict(SimpleMatrix inputMatrix) {
+		return this.predict(inputMatrix, 0);	
+	}
+	
+	public SimpleMatrix predict(SimpleMatrix inputMatrix, double explorationChance){
 		SimpleMatrix prediction = super.predict(inputMatrix);
 		
 		//Do reward correlation
@@ -39,10 +44,18 @@ public class Decider extends Predictor_VOMM {
 			correlationMatrix = correlator.correlateReward(inputMatrix, internalReward, 0.1);
 		}
 		
-		//Bias prediction toward a better choice
-		SimpleMatrix decision = bias.biasFBSpatialOutput(prediction, correlationMatrix, 0);
+		SimpleMatrix decision = null;
+		if (rand.nextDouble() < explorationChance){
+			//Choose random action
+			decision = new SimpleMatrix(prediction);
+			decision.set(0);
+			decision.set(rand.nextInt(decision.getNumElements()), 1);
+		} else {
+			//Bias prediction toward a better choice
+			decision = bias.biasFBSpatialOutput(prediction, correlationMatrix, 0);
+		}
 		probabilityMatrix = decision;
-		return decision;		
+		return decision;
 	}
 	
 	public void giveExternalReward(double reward){
@@ -52,5 +65,7 @@ public class Decider extends Predictor_VOMM {
 	public void printCorrelationMatrix(){
 		if (correlationMatrix != null) correlationMatrix.print();
 	}
+	
+	
 
 }

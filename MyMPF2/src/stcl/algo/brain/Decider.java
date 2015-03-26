49bb8c6646496ce5866d53b8e068f0f07132a837
Simon <Simon@tcpost.dk>
2015-03-26 12:34:58 +0200

    Add decider to be use by neocortical units

---
+++ MyMPF2/src/stcl/algo/brain/Decider.java
@@ -0,0 +1,63 @@
+package stcl.algo.brain;
+
+import org.ejml.simple.SimpleMatrix;
+
+import stcl.algo.util.Normalizer;
+
+public class Decider {
+	
+	private SimpleMatrix correlationMatrix;
+	private SimpleMatrix stateProbabilitiesBefore;
+	private int numPossibleStates;
+	private int numPossibleActions;
+	private double decayFactor;
+	
+	public Decider(int numPossibleActions, int numPossibleStates, double decayFactor) {
+		correlationMatrix = new SimpleMatrix(numPossibleActions, numPossibleStates);
+		this.numPossibleActions = numPossibleActions;
+		this.numPossibleStates = numPossibleStates;
+		this.decayFactor = decayFactor;
+	}
+	
+	public int decideNextAction(SimpleMatrix currentStateProbabilities, int actionToGetHere, double reward){
+		correlateActionAndReward(actionToGetHere, reward);
+		int actionToDo = chooseBestAction(currentStateProbabilities);
+		stateProbabilitiesBefore = currentStateProbabilities;
+		return actionToDo;		
+	}
+	
+	private int chooseBestAction(SimpleMatrix currentStateProbabilities){
+		int bestAction = -1;
+		double highestReward = Double.NEGATIVE_INFINITY;
+		SimpleMatrix stateVector = new SimpleMatrix(1, numPossibleStates, true, currentStateProbabilities.getMatrix().data);
+		for (int action = 0; action < numPossibleActions; action++){
+			SimpleMatrix correlationVector = correlationMatrix.extractVector(true, action);
+			SimpleMatrix rewardVector = correlationVector.elementMult(stateVector);
+			double reward = rewardVector.elementSum();
+			if (reward > highestReward){
+				highestReward = reward;
+				bestAction = action;
+			}
+		}
+		return bestAction;
+	}
+	
+	private void correlateActionAndReward(int actionToGetHere, double reward){
+		//Correlate state we were in before with the action done and reward received
+		SimpleMatrix stateVector = new SimpleMatrix(1, numPossibleStates, true, stateProbabilitiesBefore.getMatrix().data);
+		SimpleMatrix correlationVector = correlationMatrix.extractVector(true, actionToGetHere);
+		
+		//Decay old rewards
+		correlationVector.scale(1-decayFactor);
+		
+		//Add new rewards
+		correlationVector.plus(reward, stateVector);
+		
+		correlationMatrix.insertIntoThis(actionToGetHere, 0, correlationVector);
+		
+		//Normalize columns of correlationMatrix
+		correlationMatrix = Normalizer.normalizeColumns(correlationMatrix);
+
+	}
+
+}

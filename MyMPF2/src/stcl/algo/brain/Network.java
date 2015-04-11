19dad3de51157a6e219173e996637e84d0a1c97f
Simon <Simon@tcpost.dk>
2015-04-11 10:38:09 +0300

    No lines between groups in network file

--- MyMPF2/src/stcl/algo/brain/Network.java
+++ MyMPF2/src/stcl/algo/brain/Network.java
@@ -213,14 +213,11 @@ public class Network {
 			}
 		}
 		
-		buffer.append("\n");
 		buffer.append("Connections\n");
 		for (int[] conn : connections){
 			buffer.append(conn[0] + " --> " + conn[1] + "\n");
 		}
-		
-		buffer.append("\n");
-		
+
 		buffer.append("Voter influence\n");
 		TreeMap<Integer, Double> influenceMap = actionNode.getInfluenceMap();
 		for (Integer key : influenceMap.keySet()){

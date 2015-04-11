b02384b37f3f1a7a169c0c436958ee2142a5e1fa
Simon <Simon@tcpost.dk>
2015-04-11 10:31:19 +0300

    NEtwork now created from file

--- MyMPF2/src/stcl/algo/brain/Network.java
+++ MyMPF2/src/stcl/algo/brain/Network.java
@@ -1,5 +1,9 @@
 package stcl.algo.brain;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.TreeMap;
@@ -29,59 +33,83 @@ public class Network {
 	
 	/**
 	 * Use this constructor to create a NEtwork based on the string created by the toString() method
-	 * @param s
+	 * @param networkFileName
 	 * @param rand
+	 * @throws FileNotFoundException 
 	 */
-	public Network(String s, Random rand) {
+	public Network(String networkFileName, Random rand) throws FileNotFoundException {
 		sensorLayer = new ArrayList<Sensor>();
 		unitLayers = new ArrayList<ArrayList<UnitNode>>();
 		unitNodes = new ArrayList<UnitNode>();
 		nodes = new ArrayList<Node>();
 		
-		String[] lines = s.split("\n");
-		int counter = 0; 
-		//Go to first node
-		String tmp = "";
-		while(!tmp.equalsIgnoreCase("Nodes\n")){
-			tmp = lines[counter];
-			counter++;
-		}
+		BufferedReader reader = new BufferedReader( new FileReader (networkFileName));
+		String line = null;
 		
-		//Add basic nodes nodes
-		String line =lines[counter];
-		NodeFactory factory = new NodeFactory();
-		while (!line.equalsIgnoreCase("Connections\n")){
-			Node n = factory.buildNode(line, rand);
-			switch(n.getType()){
-			case SENSOR: this.addSensor((Sensor) n); break;
-			case UNIT: this.addUnitNode((UnitNode) n, n.getLayer()); break;			
-			case ACTION: this.setActionNode((ActionNode) n); break;
-			}
-			counter++;
-			line =lines[counter];
-		}
-		
-		//Connect nodes
-		while (!line.equalsIgnoreCase("Voter influence\n")){
-			String[] arr = line.split(" --> ");
-			int childID = Integer.parseInt(arr[0]);
-			int parentID = Integer.parseInt(arr[1]);
-			Node child = null;
-			Node parent = null;
-			int nodeCounter = 0;
-			while(child == null || parent == null){
-				Node n = nodes.get(nodeCounter);
-				if (n.getID() == childID) child = n;
-				if (n.getID() == parentID) parent = n;
-				nodeCounter++;
+		boolean lineFound = false;
+		try {
+			//Go to first node
+			while( ( line = reader.readLine() ) != null && ! lineFound) {
+				lineFound = line.equalsIgnoreCase("Nodes");
 			}
 			
-			child.setParent(parent);
-			parent.addChild(child);
+			//Create nodes
+			NodeFactory factory = new NodeFactory();
+			while( ( line = reader.readLine() ) != null && ! lineFound) {
+				lineFound = line.equalsIgnoreCase("Connections");
+				if (!lineFound){
+					Node n = factory.buildNode(line, rand);
+					switch(n.getType()){
+					case SENSOR: this.addSensor((Sensor) n); break;
+					case UNIT: this.addUnitNode((UnitNode) n, n.getLayer()); break;			
+					case ACTION: this.setActionNode((ActionNode) n); break;
+					}
+				}
+			}
 			
-			counter++;
-			line = lines[counter];
+			//Connect nodes
+			while( ( line = reader.readLine() ) != null && ! lineFound) {
+				lineFound = line.equalsIgnoreCase("Voter influence");
+				if (!lineFound){
+					String[] arr = line.split(" --> ");
+					int childID = Integer.parseInt(arr[0]);
+					int parentID = Integer.parseInt(arr[1]);
+					Node child = null;
+					Node parent = null;
+					int nodeCounter = 0;
+					while(child == null || parent == null){
+						Node n = nodes.get(nodeCounter);
+						if (n.getID() == childID) child = n;
+						if (n.getID() == parentID) parent = n;
+						nodeCounter++;
+					}
+					
+					child.setParent(parent);
+					parent.addChild(child);
+				}
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+	
+	private String readFile( String file ) throws IOException {
+	    BufferedReader reader = new BufferedReader( new FileReader (file));
+	    String         line = null;
+	   String s = "";
+	    // StringBuilder  stringBuilder = new StringBuilder();
+	    String         ls = System.getProperty("line.separator");
+
+	    while( ( line = reader.readLine() ) != null ) {
+	        s += line;
+	        s+= ls;
+	    	//stringBuilder.append( line );
+	       // stringBuilder.append( ls );
+	    }
+	    reader.close();
+	    return s;//stringBuilder.toString();
 	}
 	
 	public void addSensor(Sensor sensor){

d693efc05c083781870022193ed04bf6365126cc
Simon <Simon@tcpost.dk>
2015-04-21 13:51:01 +0300

    Now freezing parameter vector in episodes

---
+++ MyMPF2/src/stcl/fun/qlearning/NeuralNet.java
@@ -0,0 +1,11 @@
+package stcl.fun.qlearning;
+
+public class NeuralNet {
+	
+	
+
+	public NeuralNet(int inputLayerSize, int outputLayerSize, int hiddenLayerSize, int numHiddenLayers) {
+		// TODO Auto-generated constructor stub
+	}
+
+}

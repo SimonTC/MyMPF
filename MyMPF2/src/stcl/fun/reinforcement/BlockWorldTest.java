6edaafb4e3a701428e0224a5e3327f32284996cf
Simon <Simon@tcpost.dk>
2015-05-09 10:55:53 +0300

    Fixed conflicts

--- MyMPF2/src/stcl/fun/reinforcement/BlockWorldTest.java
+++ MyMPF2/src/stcl/fun/reinforcement/BlockWorldTest.java
@@ -6,124 +6,6 @@ import org.ejml.simple.SimpleMatrix;
 
 import stcl.algo.brain.ActionDecider;
 
-public class BlockWorldTest {
-	private enum ACTIONS {N,S,E,W};
-	private Random rand = new Random();
-	private SimpleMatrix world;
-	private State start, end;
-	private ActionDecider agent;
-	private int worldSize;
-	
-	public static void main(String[] args){
-		BlockWorldTest bwt = new BlockWorldTest(4);
-		bwt.run(100);
-	}
-	
-	public BlockWorldTest(int worldSize){
-		this.worldSize = worldSize;
-		setupWorld(worldSize);
-		setupAgent(worldSize);
-	}
-	
-	private void setupAgent(int worldSize){
-		agent = new ActionDecider(4, world.getNumElements(), 0.9);
-	}
-	
-	private void setupWorld(int worldSize){
-		world = new SimpleMatrix(worldSize, worldSize);
-		int startID = rand.nextInt(worldSize * worldSize);
-		int endID = rand.nextInt(worldSize * worldSize);
-		while (endID == startID){
-			endID = rand.nextInt(worldSize * worldSize);
-		};
-		
-		start = new State(startID, world);
-		end = new State(endID, world);
-		System.out.println("Start: (" + start.col + "," + start.row + ") ID: " + start.id);
-		System.out.println("End: (" + end.col + "," + end.row + ") ID: " + end.id);
-		world.set(0.001);
-		world.set(endID, 1);
-
-	}
-	
-	public void run(int numEpisodes){
-		int minSteps = Integer.MAX_VALUE;
-		for (int i = 1; i <= numEpisodes; i++){
-			int steps = runEpisode(1 - (double) i / numEpisodes);
-			if (steps < minSteps) minSteps = steps;
-			System.out.println("Finished episode " + i + " in " + steps + " steps");	
-			/*
-			System.out.println();
-			System.out.println("Q matrix: ");
-			agent.printQMatrix();
-			System.out.println();
-			
-			System.out.println("Trace matrix:");
-			agent.printTraceMatrix();
-			*/
-		}
-		
-		//agent.printQMatrix();
-		
-		//Evaluation
-		int steps = runEpisode(0);
-		System.out.println("Finished evaluation in " + steps + " steps");
-		System.out.println("Minimum number of steps used: " + minSteps);
-		
-		agent.printCorrelationMatrix();
-	}
-	
-	private int runEpisode(double explorationChance){
-		boolean goalFound = false;
-		ACTIONS action = ACTIONS.N;
-		State state = start;
-		double reward = 0;
-		int counter = 0;
-		//System.out.println("Starts in: (" + state.getCol() + ", " + state.getRow() + ")");
-		while (!goalFound && counter < 1000){
-			SimpleMatrix stateProbs = new SimpleMatrix(worldSize, worldSize);
-			stateProbs.set(state.getID(), 1);			
-			agent.feedForward(stateProbs, action.ordinal(), reward);
-			
-			State nextState = move(state, action);
-			state = nextState;
-			
-			if(state.equals(end)) goalFound = true;
-			reward = world.get(state.id);
-			stateProbs = new SimpleMatrix(worldSize, worldSize);
-			stateProbs.set(state.getID(), 1);
-			int nextAction = agent.feedback(stateProbs);
-			if (rand.nextDouble() < explorationChance){
-				nextAction = rand.nextInt(ACTIONS.values().length);
-			}
-			action = ACTIONS.values()[nextAction];	
-			counter++;
-		}
-		SimpleMatrix stateProbs = new SimpleMatrix(worldSize, worldSize);
-		stateProbs.set(state.getID(), 1);
-		agent.feedForward(stateProbs, action.ordinal(), reward); //Need to give it the reward for finishing
-		
-		return counter;
-	}
-	
-	private State move(State state, ACTIONS action){
-		int rowChange = 0, colChange = 0;
-		switch(action){
-		case E: rowChange = 0; colChange = 1;  break;
-		case N: rowChange = 1; colChange = 0;  break;
-		case S: rowChange = -1; colChange = 0;  break;
-		case W: rowChange = 0; colChange = -1;  break;		
-		}
-		
-		int newRow = state.getRow() + rowChange;
-		int newCol = state.getCol() + colChange;
-		
-		if(newRow < 0 || newRow > world.numRows() - 1) newRow = state.getRow();
-		if(newCol < 0 || newCol > world.numCols() - 1) newCol = state.getCol();
-		
-		State newState = new State(newRow, newCol, world);
-		return newState;
-=======
 
 public class BlockWorldTest {
 	private SimpleMatrix world;
@@ -260,7 +142,6 @@ public class BlockWorldTest {
 	
 	public int getNumStates(){
 		return world.getNumElements();
->>>>>>> refs/remotes/origin/eligibilityTraces
 	}
 	
 	private class State{

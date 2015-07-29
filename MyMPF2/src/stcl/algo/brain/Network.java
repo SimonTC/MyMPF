824182795ef8a643a1d8c2a30053491b7c06807e
Simon <Simon@tcpost.dk>
2015-07-29 14:12:22 +0300

    Network now reinitialized before detailed description is written

--- MyMPF2/src/stcl/algo/brain/Network.java
+++ MyMPF2/src/stcl/algo/brain/Network.java
@@ -250,6 +250,7 @@ public class Network implements Serializable{
 		}
 		
 		buffer.append("----Detailed description----\n");
+		this.reinitialize(); //Need to reinitialize to make sure we get the start values and not the learned values
 		for (Node n : nodes){
 			buffer.append("Node " + n.getID() + "\n");
 			buffer.append(n.toInitializationString());
